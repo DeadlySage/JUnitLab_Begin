7f84aaae43587af9d1574fa1061a8de753218b79
Adrian Tandiono <adrian.hartanto159@gmail.com>
2021-03-15 21:51:36 -0700

    added 3 test to cover all possible exceptions
    added 2 test to cover invalid transaction and amount due

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -51,6 +51,58 @@ public class GiftCardTest {
 		
 		x = "Remaining Balance: " + String.format("%6.2f", balance - 20.0);
 		
-		assertEquals("deduct()", x, card.deduct(20.0));
+		assertEquals("deduct() 20 from 100.00", x, card.deduct(20.0));
+	}
+	
+	@Test
+	public void deduct_InvalidTransaction()
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		String x;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		x = "Invalid Transaction";
+		
+		assertEquals("deduct() - 20.0 from 100.00", x, card.deduct(-20.0));
+	}
+	
+	@Test
+	public void deduct_AmountDue()
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		String x;
+		
+		issuingStore = 1337;
+		balance = 0.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		x = "Amount Due: " + String.format("%6.2f", 20.0);
+		
+		assertEquals("deduct() 20 from 0.00", x, card.deduct(20.0));
+	}
+	
+	@Test
+	public void constructor_IlligalBalance() 
+	{
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(1,-100.00);});
+	}
+	
+	@Test
+	public void constructor_IncorrectID_Low()
+	{
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(-1,100.00);});
+	}
+	
+	@Test
+	public void constructor_IncorrectID_High()
+	{
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(10000,100.00);});
 	}
 }
